185434471ca64bdec30e248162804856c9e39035
systemworebel <sistemas@worebel.com>
2015-08-19 16:10:58 -0500

    Modificandoarchivo conec.php, configurando la base de datos a localhost

--- js/conec.php
+++ js/conec.php
@@ -1,8 +1,8 @@
 <?php
 function conectarse() 
 { 
-   $conn=mysql_connect('db543248245.db.1and1.com:3306', 'dbo543248245', '89rfvcde_') or die('No se pudo conectar: ' . mysql_error());
-		mysql_select_db('db543248245',$conn) or die('No se pudo seleccionar la base de datos');
+   $conn=mysql_connect('localhost', 'root', '') or die('No se pudo conectar: ' . mysql_error());
+		mysql_select_db('coomeb',$conn) or die('No se pudo seleccionar la base de datos');
   return $conn;
 } 
 ?>
